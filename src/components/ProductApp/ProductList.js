1582e82b837c4e5e2d681403a392ea959469286f
Melaniaift <melaniaiftinchi@gmail.com>
2024-10-03 16:41:57 +0300

    Create a custom fetch hook for gettiing data

--- src/components/ProductApp/ProductList.js
+++ src/components/ProductApp/ProductList.js
@@ -1,40 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import './ProductList.css'
+import { useFetch } from "../hooks/useFetch";
 
 export const ProductList = () => {
-    const [products, setProducts] = useState([]);
     const [url, setUrl] = useState("http://localhost:8000/products")
-    const [counter, setCounter] = useState(0)
-
-    //infinite loop
-    // fetch("http://localhost:8000/products")
-    //     .then(response => response.json())
-    //     .then(data => setProducts(data));
-
-    // to avoid side effects, you can try useEffect function
-    // if between [] is a variable and it is changed, then it will re-execute this function
-    // if there is no dependency, then the fetch will be executed for the first time entering the page
-    useEffect(() => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => setProducts(data));
-    }, [url]);
-
-    useEffect(() => {
-        console.error(counter);
-    }, [counter]);
+    const { data: products } = useFetch(url);
 
     return (<>
-
         <div className="button-group">
-            <button onClick={() => setCounter(counter + 1)}>{counter}</button>
             <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
             <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock</button>
         </div>
         <section>
-
-
-            {products.map((product) => (
+            {products && products.map((product) => (
                 <div className="card" key={product.id}>
                     <p className="id">{product.name}</p>
                     <p className="info">
